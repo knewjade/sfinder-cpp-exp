d5593dce13a538f81559069c64e2f10e9e2cde81
knewjade <knewjade@gmail.com>
2019-09-19 19:38:52 +0900

    Update build define

--- build/vars/sfinder_exp_main.cmake
+++ build/vars/sfinder_exp_main.cmake
@@ -2,6 +2,8 @@
 set(sfinder_exp_main_SOURCE_DIR ${sfinder_exp_SOURCE_DIR}/src)
 
 file(GLOB sfinder_exp_main_SRC
+        ../../src/lib/*.cpp
+        ../../src/lib/*.hpp
         ../../src/*.cpp
         ../../src/*.hpp
         )
